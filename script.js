7a426b5557ec9c563074b2662e878396944a72d7
Adhil3214 <adhilv714@gmail.com>
2025-03-09 19:17:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get username
+    let username = document.getElementById("username").value;
+
+    // Show profile section
+    document.getElementById("profileContainer").style.display = "block";
+    document.getElementById("userNameDisplay").innerText = username;
+
+    // Hide login form
+    document.querySelector(".login-container").style.display = "none";
+});
+
